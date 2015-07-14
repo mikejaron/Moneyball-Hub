759055b4377c0b5d80ea81b0be51ca542d27a830
Michael Jaron <mjaron@mjaron-macbookpro.roam.corp.google.com>
2015-07-14 12:36:20 -0700

    simplified the code, and got it working

--- src/api_call.py
+++ src/api_call.py
@@ -2,10 +2,11 @@ import csv
 import json
 import requests
 import pprint
+import time
 
-def scroll_paginated_query(query, url, page_size=3000, params=None, retry_count_on_fail=3):
+def scroll_paginated_query(query, url, page_size=3000, params=None, AUTH = ('mjaron','canudigit') ,retry_count_on_fail=3):
     """
-    INPUT: url=string of apit to query, query=ealsticsearch string, page_size=integer
+    INPUT: url=string of apit to query, query=elasticsearch string, page_size=integer
     OUTPUT: This is the main funtion to query the api, and split it into pages to make things
     easier.
     """ 
@@ -18,7 +19,6 @@ def scroll_paginated_query(query, url, page_size=3000, params=None, retry_count_
     requery = True
 
     while keep_getting_pages:
-
 
         # check if valid response
         num_tries = 0
@@ -86,7 +86,6 @@ def scroll_paginated_query(query, url, page_size=3000, params=None, retry_count_
                 # query failed. retry again and again until the retry limit
                 requery = True
 
-
             elif num_tries > retry_count_on_fail:
 
                 msg = "Query failed."
@@ -99,194 +98,61 @@ def scroll_paginated_query(query, url, page_size=3000, params=None, retry_count_
         yield results_page
 
 
-def get_info_on_sentences_with_a_tag(tag='130', begin_date='2015-01-01', end_date='2015-02-01', search_url="http://104.197.14.37/api/v1/sentences/dd/search", save_directory='.'):
+def get_info_on_sentences_with_a_tag(tag='130', begin_date='2012-01-01t00:00:00', end_date='2015-07-30t00:00:00', search_url="http://104.197.14.37/api/v1/sentences/dd/search", save_directory=''):
     """
-    INPUT: query=elasticsearch string, search_url=url of api string, save_directory=string
+    INPUT: begin_date & end_date= date in 'y-m-dth:m:s' format, search_url=url of api string, 
+    save_directory=string
     OUTPUT: This calls previous function and gets the json from that, parses it and saves it
     to a csv
     """
+    ### initialize csv
+    writefile = save_directory+tag+".csv"
+    fieldnames = ['timestamp','source', 'polarity', 'subjectivity', 'text']
+    with open( writefile, 'w' ) as f:
+        writer = csv.writer(f)
+        writer.writerow(fieldnames)
 
     query = r'{"query": {"filtered": {"filter":{"range":{"timestamp":{"gt":'+'"'+begin_date+'"'+', "lt":'+'"'+end_date+'"'+'}}}, "query": {"filtered": {"filter": { "term": { "tags": '+tag+' }}}} }}}'
-    
+    doc1 = []
     print search_url
+    print query
     for page in scroll_paginated_query(query=query, url=search_url):
-    #     for page in scroll_paginated_query(doc_search_url, query):
         main_source1 = [hit['_source']["source"] for hit in page['hits']['hits']]
-        id = [hit['_source']["_id"] for hit in page['hits']['hits']]
-        if sent_search == 1:
-            documents = [hit['_source']["doc_id"] for hit in page['hits']['hits']]
+        documents = [hit['_source']["doc_id"] for hit in page['hits']['hits']]
         texts = [hit['_source']["text"]for hit in page['hits']['hits']]
         polarities = [hit['_source']["sentiment"]["polarity"]for hit in page['hits']['hits']]
         subjectivities = [hit['_source']["sentiment"]["subjectivity"]for hit in page['hits']['hits']]
         timestamps = [hit['_source']["timestamp"]for hit in page['hits']['hits']]
 
-        ####### For Yelp
-        # star = [hit['_source']["parent"]for hit in page['hits']['hits']]
-        #       for temp in star:
-        #           documentstring = str(temp)
-        #           stars.append(documentstring[:])
         main_source = []
         for temp in main_source1:
             string = str(temp)
-            url = string.find("https://")
-            url1 = string.find("http://")
-            url2 = string.find("www.")
-            end1 = string.find(".net")
-            end = string.find(".com")
-            if url >= 0:
-                if url2 >= 0:
-                    if end >= 0:
-                        main_source.append(string[12:end])
-                    elif end1 >= 0:
-                        main_source.append(string[12:end1])
-                else:
-                    if end >= 0:
-                        main_source.append(string[8:end])
-                    elif end1 >= 0:
-                        main_source.append(string[8:end1])
-            elif url1 >= 0:
-                if url2 >= 0:
-                    if end >= 0:
-                        main_source.append(string[11:end])
-                    elif end1 >= 0:
-                        main_source.append(string[11:end1])
-                else:
-                    if end >= 0:
-                        main_source.append(string[7:end])
-                    elif end1 >= 0:
-                        main_source.append(string[7:end1])   
-            else:
-                main_source.append(string[:]) 
+            main_source.append(string[:]) 
 
 
-        data1 = []
+        date_list = []
         for temp in timestamps:
             documentstring = str(temp)
-            data1.append(documentstring[:])
-        ### this was to get only the date  data1.append(documentstring[:10])
-        data2 = []
+            date_list.append(documentstring[:]) ### this was to get only the date  date_list.append(documentstring[:10])
+        
+        pol_list = []
         for temp in polarities:
             documentstring = str(temp)
-            data2.append(documentstring[:])
-        data3 = []
+            pol_list.append(documentstring[:])
+        sub_list = []
         for temp in subjectivities:
             documentstring = str(temp)
-            data3.append(documentstring[:])
+            sub_list.append(documentstring[:])
         textlist = []
         for temp in texts:
             documentstring = temp.encode('utf-8')
             textlist.append(documentstring[:])
-        cat = []
-        top = []
-        type = []
-        data5 = []
-        if sent_search == 1:
-            for temp in documents:
-                cat.append(category)
-                top.append(category)
-                documentstring = str(temp)
-                doc1.append(documentstring[:])
-                dash = documentstring.find("-")
-                string = documentstring[:dash]
-                split = string.find("tweet")
-                if split == 0:
-                    data5.append("twitter")
-                    type.append(string[:])
-                else:
-                    if split == -1:
-                        split = string.find("Article")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("sDiscussion")
-                        split1 = split + 1
-                    if split == -1:
-                        split = string.find("Discussion")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Forum")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Blog")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Comment")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("NewsItem")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Review")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Feature")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("ForumsMessage")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("ForumPost")
-                        split1 = split
+        #### this is to save to a main list of all doc ids to use later for children
+        for temp in documents:
+            documentstring = str(temp)
+            doc1.append(documentstring[:])
 
-                    data5.append(string[:split])
-                    type.append(string[split1:])
-
-            send_id = []
-            for temp in id:
-                documentstring = str(temp)
-                sent_id.append(documentstring[:])
-        elif doc_search == 1:
-            for temp in id:
-                cat.append(category)
-                top.append(category)
-                documentstring = str(temp)
-                doc1.append(documentstring[:])
-                dash = documentstring.find("-")
-                string = documentstring[:dash]
-                split = string.find("tweet")
-                if split == 0:
-                    data5.append("twitter")
-                    type.append(string[:])
-                else:
-                    if split == -1:
-                        split = string.find("Article")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("sDiscussion")
-                        split1 = split + 1
-                    if split == -1:
-                        split = string.find("Discussion")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Forum")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Blog")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Comment")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("NewsItem")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Review")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("Feature")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("ForumsMessage")
-                        split1 = split
-                    if split == -1:
-                        split = string.find("ForumPost")
-                        split1 = split
-
-                    data5.append(string[:split])
-                    type.append(string[split1:])
-                    sent_id.append(string[:split])
-
-        writefile = save_directory
+        writefile = save_directory+tag+".csv"
         with open( writefile, 'a' ) as f:
             writer = csv.writer(f)
-#                 writer.writerows(zip(top, cat, data1, sent_id, data5, type, data2, data3, textlist))
-            writer.writerows(zip(top, cat, data1, sent_id, main_source, type, data2, data3, textlist))
+            writer.writerows(zip(date_list, main_source, pol_list, sub_list, textlist))
